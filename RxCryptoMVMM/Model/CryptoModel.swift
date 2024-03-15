4d6d713aad954401540b2990f100cb4c220fdd0d
Xenondq <almaz.eyupensar@gmail.com>
2024-03-15 12:05:59 +0000

    model added

---
+++ RxCryptoMVMM/Model/CryptoModel.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoModel.swift
+//  RxCryptoMVMM
+//
+//  Created by Eyup Ensar Almaz on 15/03/2024.
+//
+
+import Foundation
+
+struct Crypto: Decodable{
+    let currency: String
+    let price: String
+}
